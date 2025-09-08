310aa055df1bd257cdf3e5c5b2b9f23f54680509
Craig McIlwrath <craigmc08@gmail.com>
2025-09-08 07:05:31 -0400

    use system conf

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A simple system flake using some Aux defaults";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.05";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
   outputs =
     inputs@{ nixpkgs, ... }:
