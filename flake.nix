3c1fb884ec32dc35011ac4f26e160b24b495d658
Craig McIlwrath <craigmc08@gmail.com>
2025-09-08 07:01:54 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A simple system flake using some Aux defaults";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.05";
+
+  outputs =
+    inputs@{ nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      hostName = "lauma"; # Set this variable equal to your hostName
+    in
+    {
+      nixosConfigurations.${hostName} = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./configuration.nix
+
+          {
+            networking.hostName = hostName;
+            nixpkgs.hostPlatform = system;
+          }
+        ];
+
+        specialArgs = {
+          inherit inputs;
+        };
+      };
+    };
+}
